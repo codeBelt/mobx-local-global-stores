c74045b3674de9b11317f448f9226124064ebbfb
robertsavian <robertsavian@gmail.com>
2021-01-13 22:07:21 -0600

    initial

---
+++ src/components/shared/main-nav/MainNav.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Menu, Segment } from 'semantic-ui-react';
+import { MenuNavLink } from './components/MenuNavLink';
+import { Routes, RoutesDynamicKey } from '../../../constants/Routes';
+import { defaultShowId } from '../../../domains/shows/shows.constants';
+import { useRouter } from 'next/router';
+
+interface IProps {}
+
+export const MainNav: React.FC<IProps> = (props) => {
+  const router = useRouter();
+
+  return (
+    <Segment inverted={true}>
+      <Menu inverted={true} pointing={true} secondary={true}>
+        <Menu.Item as={MenuNavLink} href={Routes.Index} name="Home" active={router.route === Routes.Index} />
+        <Menu.Item
+          as={MenuNavLink}
+          href={Routes.Episodes_Id.replace(RoutesDynamicKey.EpisodeId, defaultShowId)}
+          name="Episodes"
+          active={router.route === Routes.Episodes_Id}
+        />
+        <Menu.Item as={MenuNavLink} href={Routes.About} name="About" active={router.route === Routes.About} />
+      </Menu>
+    </Segment>
+  );
+};
---
+++ src/domains/shows/shows.constants.ts
@@ -0,0 +1,1 @@
+export const defaultShowId = '74';
